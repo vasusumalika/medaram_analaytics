2162246fe479efdd9623bbcd0e1ea0556876e409
adabalaSreeVidya <sreevidya@outliers.tech>
2024-01-31 23:27:56 +0530

    Designed & developed User model listing, add & edit operations

--- app/urls.py
+++ app/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('do_login', views.do_login, name="do_login"),
     path('dashboard', dashboard, name='dashboard'),
     path('logout_user', views.logout_user, name="logout_user"),
+    path('users/list', views.users_list, name='users_list'),
+    path('users/add', views.user_add, name='user_add'),
+    path('users/edit', views.user_edit, name='user_edit'),
+    path('users/update', views.user_update, name='user_update'),
 ]
