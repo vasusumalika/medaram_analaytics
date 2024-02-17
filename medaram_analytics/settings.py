d3f462f24c7249bdcf67dbdd6b6c36a229f612fa
adabalaSreeVidya <sreevidya@outliers.tech>
2024-02-17 09:29:08 +0530

    Fixed issues.

--- medaram_analytics/settings.py
+++ medaram_analytics/settings.py
@@ -166,9 +166,9 @@ STATICFILES_DIRS = [
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
